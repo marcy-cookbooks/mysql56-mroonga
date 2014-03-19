a2c6d9bdb9c1c2694092e1eeb4560042c339db86
Marcy <marcy9114@gmail.com>
2014-03-19 16:51:16 +0900

    Add template of configure file

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
-default['mysql56-mroonga']['mysql_version']   = "5.6.16-1"
-default['mysql56-mroonga']['mroonga_version'] = "4.00-2"
-default['mysql56-mroonga']['root_password']   = "password"
+default['mysql56-mroonga']['mysql_version']          = "5.6.16-1"
+default['mysql56-mroonga']['mroonga_version']        = "4.00-2"
+default['mysql56-mroonga']['root_password']          = "password"
+default['mysql56-mroonga']['mroonga_default_parser'] = "TokenMecab"
